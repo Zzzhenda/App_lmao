53e305b7d9321f1081ee4bb4a7c8ed8caca4537c
Zzzhenda <151049778+Zzzhenda@users.noreply.github.com>
2025-10-06 16:12:23 -0300

    Add files via upload

---
+++ app_lmao/app/src/main/java/com/example/app_lmao/screens/productosScreen.kt
@@ -0,0 +1,18 @@
+package com.example.app_lmao.screens
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.icons.Icons
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.filled.Search
+
+
+
+
+
+
+
+
+
+class productosScreen {
+
+}
